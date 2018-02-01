83cd2bc62c932ae4071015953f61abbd55a4504f
Stavros Kefaleas <kefaleas.stavros@gmail.com>
2018-02-01 12:01:50 +0200

    3nd commit

--- routes/messages.js
+++ routes/messages.js
@@ -7,7 +7,7 @@ router.post('/', function(req, res, next) {
     const message = new Message({
         content: req.body.content
     });
-    console.log(rec);
+    console.log(req);
     message.save(function(err, result) {
         if (err)
             return res.status(500).json({
